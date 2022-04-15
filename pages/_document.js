b5f354950a683cb599b28c285ebfeb5ec56fa474
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-04-15 15:40:49 +0200

    up

--- pages/_document.js
+++ pages/_document.js
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
         <Html>
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet" />
         </Head>
         <body>
